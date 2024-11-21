d955cba87a96c60ef909ba9802cd238a04516761
SalinasJason <jasonsalinas770@gmail.com>
2024-11-20 16:11:38 -0800

    added inputText

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-
 let form = document.getElementById('myForm'); // Declaring a variable name form and using getElementById to find the html element.
 
 form.addEventListener('submit', function(event) { // Adding a eventlistener "submit" and event handler.
   event.preventDefault();  // Prevents the form default action
   console.log('Form default prevented:', event.defaultPrevented);  // Console logs true if the default action was prevented
+
+  let inputText = document.getElementById('inputText'); // Declaring a variable named inputText and using getElementById to find the input element
+  console.log('Input value:', inputText.value); // Console logs the value of the input text element
 });
 
 
